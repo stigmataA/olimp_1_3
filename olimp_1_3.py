9015374cf974fbd6567414d3193c336acbc15fd3
stigmataA <r.muhutdinov@gmail.com>
2024-10-28 16:30:47 +0300

    Add files via upload

---
+++ olimp_1_3.py
@@ -0,0 +1,18 @@
+A = float(input(f'Введите значение давления, А: '))
+
+epsilon = float(input(f'Введите значение отклонения: '))
+
+N = int(input(f'Введите время тестирования: '))
+
+unacceptable_count = 0
+
+for _ in range(N):
+
+    reading = float(input(f'Показания датчика: '))
+
+    deviation = abs(reading - A)
+
+    if deviation > epsilon:
+        unacceptable_count += 1
+
+print(f'Количество отклонений: {unacceptable_count}')
